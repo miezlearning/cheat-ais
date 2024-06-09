77502911a08864735925926d120bbadc88d23969
miezlearning <gottfriedemptiness@gmail.com>
2024-06-10 03:31:12 +0800

    Tambah fitur, dan perubahan Additional : Tambah fitur reset kuesioner Additional : Tambah fitur jika membatalkan pilihan Modified : penambahan menu pada switch case dan kalimat pada inputan.

--- Hati hati.js
+++ Hati hati.js
@@ -1,4 +1,4 @@
-const pilihan = prompt("Pilih metode pengisian kuesioner:\n1. Isi dengan nilai 1\n2. Isi dengan nilai 2\n3. Isi dengan nilai 3\n4. Isi dengan nilai 4\n5. Isi dengan nilai 5\n6. Isi dengan nilai acak");
+const pilihan = prompt("Pilih metode pengisian kuesioner:\n1. Isi dengan nilai 1\n2. Isi dengan nilai 2\n3. Isi dengan nilai 3\n4. Isi dengan nilai 4\n5. Isi dengan nilai 5\n6. Isi dengan nilai acak\n7. Reset kuisioner.");
 
 // Kuisioner berdasarkan pilihan mahasiswa.
 function isiKuesioner(nilai) {
@@ -36,28 +36,51 @@ function isiKuesionerAcak(rentang) {
     console.log(`%cKuesioner telah diisi otomatis dengan nilai acak dalam rentang ${rentang.join('-')} menggunakan acak ${metode}.`, 'color: blue; font-weight: bold;');
 }
 
-switch (pilihan) {
-    case '1': //Full Skala 1
-    case '2': // Full Skala 2
-    case '3': // Full Skala 3
-    case '4': // Full Skala 4
-    case '5': // Full Skala 5
-        isiKuesioner(pilihan);
-        break;
+function resetKuesioner() {
+    const skalaPenilaian = document.querySelectorAll('.form-check-input');
+    skalaPenilaian.forEach(button => {
+        button.checked = false;
+    });
+    console.log("%cKuesioner telah direset.", 'color: orange; font-weight: bold;');
+}
 
-    case '6': // Skala Acak dengan pilihan.
-        const pilihacakan = prompt("pilih jenis pengisian acak:\n1. Acak Buruk (nilai 1-3)\n2. Acak Baik (nilai 4-5)");
-        switch (pilihacakan) {
-            case '1':
-                isiKuesionerAcak([1, 2, 3]);
-                break;
-            case '2':
-                isiKuesionerAcak([4, 5]);
-                break;
-            default:
-                console.log("%cDilihat baik baik menunya.", 'color: red; font-weight: bold;');
-        }
-        break;
-    default:
-        console.log("%cDilihat baik baik menunya.", 'color: red; font-weight: bold;');
+
+// Kondisi jika orang batal melakukan.
+if (pilihan === null) {
+    console.log("%cProses dibatalkan oleh pengguna.", 'color: red; font-weight: bold;');
+} else {
+    switch (pilihan) {
+        case '1': // Full Skala 1
+        case '2': // Full Skala 2
+        case '3': // Full Skala 3
+        case '4': // Full Skala 4
+        case '5': // Full Skala 5
+            isiKuesioner(pilihan);
+            break;
+
+        case '6': // Skala Acak dengan pilihan.
+            const pilihacakan = prompt("Pilih jenis pengisian acak:\n1. Acak Baik (nilai 1-3)\n2. Acak Buruk (nilai 4-5)");
+            // Kondisi jika orang batal melakukan.
+            if (pilihacakan === null) {
+                console.log("%cProses dibatalkan oleh pengguna.", 'color: red; font-weight: bold;');
+            } else {
+                switch (pilihacakan) {
+                    case '1':
+                        isiKuesionerAcak([1, 2, 3]);
+                        break;
+                    case '2':
+                        isiKuesionerAcak([4, 5]);
+                        break;
+                    default:
+                        console.log("%cDilihat baik-baik menunya.", 'color: red; font-weight: bold;');
+                }
+            }
+            break;
+        case '7': // Reset skalanya.
+            resetKuesioner();
+            break;
+        default:
+            console.log("%cDilihat baik-baik menunya.", 'color: red; font-weight: bold;');
+    }
+
 }
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Selamat Datang di Script Jahil 😅
+
+Jadi, pada script kali ini untuk memudahkan mahasiswa dalam mengisi kuisioner. Tentu ini bermanfaat apalagi generasi milenial sekarang itu orangnya mageran. Dilihat dari hasil survey yang ada, maka dari itu saya membuat script ini 😂 (termasuk saya sendiri).
+
+
+# Cara Memakai Script 🤖
+
+**DISARANKAN MENGGUNAKAN LAPTOP/PC** 💻
+
+Caranya cukup simpel jadi jangan stupid banget, tolong dipahami :
+
+1. Buka Halaman **AIS** atau [**klik aku**](https://ais.unmul.ac.id/).
+2. Buka Navbar **Perkuliahan**, setelah itu kebagian **Kartu Hasil Studi** atau [**klik aku**](https://ais.unmul.ac.id/mahasiswa/khs).
+3. Pilih **Tahun Ajar/Semester** kalian yang ingin diisi kuisionernya.
+4. Klik tombol **ISI KUISIOENER** <span style="color:orange"> (Berwarna Orange/Jingga) </span> pada dosennya 
+5. Buka **Console** pada browser kalian masing-masing.
+   > **TIPS** : Gunakan **F12** atau buka artikel [berikut](https://sko.dev/referensi/javascript/menulis-js-pada-console-browser)
+6. Jalankan script dengan menekan tombol **ENTER**.
+7. Tinggal pilih menu ketika ada *pop-up* yang muncul.
+
+
+
+### Masih tidak paham? yaudah nih videonya deh. 😤
+
+> Nanti aja yah... mau diedit dulu.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## ©Credit: [bang aldinh](https://github.com/aldinh777/cheat-sia/tree/master/source) (inspirasi dalam membuat script.)
